cb26dfcb6cfd7184da5e176eb02dd4cbf2faec92
hisanori <hisary@gmail.com>
2016-10-31 14:41:29 +0900

    Add specs

---
+++ spec/httpserver_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+require 'httpserver/version'
+require 'httpserver/options'
+require 'httpserver/server'
+require 'net/http'
+
+describe HTTPServer do
+  it 'has a version number' do
+    expect(HTTPServer::VERSION).not_to be nil
+  end
+
+  it 'can parse cli options' do
+    opts = HTTPServer::Options.get(['-a', '10.200.1.100', '-p', '9999', './doc_roottttt'])
+    expect(opts[:BindAddress]).to eq '10.200.1.100'
+    expect(opts[:DocumentRoot]).to eq './doc_roottttt'
+    expect(opts[:Port]).to eq 9999
+  end
+  
+  it 'can boot server' do
+    opt = HTTPServer::Options.get(['./spec/htdocs']);
+    opt[:Port] = 8888;
+    server = HTTPServer::Server.new(opt)
+    Thread.new {
+      server.start
+    }
+
+    Net::HTTP.start('localhost', 8888) { |http|
+      res = http.get('/test.html')
+      expect(res.code).to eq "200"
+    }
+
+    server.stop 
+  end
+end
+
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,3 @@
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "http/server"
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
+Dir::chdir(File.expand_path('../../', __FILE__))
